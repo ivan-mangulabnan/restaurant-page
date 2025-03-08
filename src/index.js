3692d861ad7a986579fafbe32c5a728ece5d7834
ivan <ivanmangulabnan19@gmail.com>
2025-03-08 23:34:23 +0800

    Add eventlisteners for modules

--- src/index.js
+++ src/index.js
@@ -1,2 +1,35 @@
 import "./styles.css";
-import { showHome } from "./scripts/home.js";
+import { mainContainer, showHome } from "./scripts/home.js";
+import { showMenu } from "./scripts/menu.js";
+
+const nav = document.querySelector(`nav`);
+const homeButton = document.querySelector(`.home`);
+const menuButton = document.querySelector(`.menu`);
+const aboutButton = document.querySelector(`.about`);
+
+function addClassPressed(event) {
+    if (![homeButton, menuButton, aboutButton].includes(event.target)) return;
+    if (event.target !== homeButton) mainContainer.classList.remove(`turn-to-grid`);
+    homeButton.classList.remove(`pressed`);
+    menuButton.classList.remove(`pressed`);
+    aboutButton.classList.remove(`pressed`);
+
+    event.target.classList.add(`pressed`);
+}
+
+nav.addEventListener(`click`, addClassPressed);
+
+document.addEventListener(`DOMContentLoaded`, () => {
+    homeButton.classList.add(`pressed`);
+    showHome();
+});
+
+homeButton.addEventListener(`click`, () => {
+    mainContainer.innerHTML = "";
+    showHome();
+});
+
+menuButton.addEventListener(`click`, () => {
+    mainContainer.innerHTML = "";
+    showMenu();
+})
